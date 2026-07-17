4446b162031dc1072bf36d5bebc7f25231d41c6e
portfolio-ss <fs50799@gmail.com>
2026-07-17 03:19:43 -0300

    Configuração automática do setup

---
+++ config.js
@@ -0,0 +1,10 @@
+const CONFIG = {
+    PROXY_URL: 'https://demolay-proxy.workers.Fs50799@gmail.com's Account.workers.dev',
+    CLIENT_ID: '897683631001-ugml9ertq7bldbtmsugcejhitav6l4dp.apps.googleusercontent.com',
+    SHEET_ID: '1jAiDtjqEZmPJsXB6IoBvIz-vGQdrRAHm5ZUtTSslDEA',
+    SCOPES: 'https://www.googleapis.com/auth/spreadsheets profile email',
+    SHEET_LINKS: 'Sheet1',
+    SHEET_CONFIG: 'Sheet2',
+    RANGE_LINKS: 'Sheet1!A:F',
+    RANGE_CONFIG: 'Sheet2!A:B',
+};
